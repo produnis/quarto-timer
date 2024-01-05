2e8a407d56d51c76f576bf43b2fa320767312b47
Joe Slam <schlarmann@produnis.de>
2024-01-05 12:02:13 +0100

    lua

---
+++ _extensions/timer/timer.lua
@@ -0,0 +1,29 @@
+function CodeBlock(block)
+    if block.classes[1] == "timer" then
+        local containerId = block.identifier
+        local timeLimit = tonumber(block.attributes["seconds"]) or 300  -- Default: 300 Sekunden
+
+        local htmlSnippet = string.format([[
+            <div id="%s"></div>
+            <script>
+                document.addEventListener("DOMContentLoaded", function () {
+                    initializeTimer("%s", %d);
+                });
+            </script>
+        ]], containerId, containerId, timeLimit)
+
+        return pandoc.RawBlock("html", htmlSnippet)
+    end
+    return block
+end
+
+-- Füge den Filter zu Pandoc hinzu
+return {
+    { CodeBlock = CodeBlock }
+}
+
+quarto.doc.add_html_dependency({
+  name = "quarto-timer",
+  version = "1.0.0",
+  scripts = {"timer.js"}
+})
